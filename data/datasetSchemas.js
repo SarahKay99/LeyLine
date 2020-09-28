0ab5cb45b81aed9ffb0011ec4ca2607b97cd9aa3
Sarah Kay <51705696+SarahKay99@users.noreply.github.com>
2020-09-28 14:11:22 +1000

    Removed submit button

--- data/datasetSchemas.js
+++ data/datasetSchemas.js
@@ -4,7 +4,8 @@ let datasetSchema = new mongoose.Schema({
     name: { type: String, required: true },
     categories: [categorySchema],
     completed: { type: Boolean, required: true },
-    commissionerID: { type: mongoose.Schema.Types.ObjectId, ref: "users" }      // Foreign Key.
+    commissionerID: { type: mongoose.Schema.Types.ObjectId, ref: "users" },      // Foreign Key.
+    fullyAnnotated: { type: Boolean, required: true }
 });
 
 module.exports = mongoose.model("dataset", datasetSchema);
