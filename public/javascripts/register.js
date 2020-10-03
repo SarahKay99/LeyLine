ccf93edb9ef1b1b0fd99054f42332d592b01067d
s3662541 <s3662541@student.rmit.edu.au>
2020-10-03 15:42:08 +1000

    Allowed users to disable optional inputs with checkbox on the register page

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
     var checkBox = $("#data-provide");
+    var checked = false;
     checkBox.click(function() {
-        
+        if (checked == false) {
+            checked = true;
+            $("#registerFirstName").prop('disabled', true);
+            $("#registerLastName").prop('disabled', true);
+        }
+        else {
+            checked = false;
+            $("#registerFirstName").prop('disabled', false);
+            $("#registerLastName").prop('disabled', false);
+        }
     });
 });
