011f05776ffec3c29f3f6155331c258c4bf33765
s3662541 <s3662541@student.rmit.edu.au>
2020-10-07 17:12:40 +1100

    Implemented alterations to sidebar functionality

--- public/javascripts/details.js
+++ public/javascripts/details.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-    var toggled = false;
-
-    $("#sidebar-toggle").click(function() {
-        if (!toggled) {
-            $("#menu-items").css("width", "85%");
-            toggled = true;
-        }
-        else {
-            $("#menu-items").css("width", "100%");
-            toggled = false;
-        }
-    });
+    
 });
