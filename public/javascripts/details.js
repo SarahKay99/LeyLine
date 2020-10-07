81300afbb863f2a103bc8a103ab9256c571d64d7
s3662541 <s3662541@student.rmit.edu.au>
2020-10-07 19:14:45 +1100

    Added functional tabs on the user details page

--- public/javascripts/details.js
+++ public/javascripts/details.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    
+    $("[id^=side-btn-]").click(function() {
+        var id = $(this).attr('id');
+        var num = id.split("-")[2];
+
+        $(`#tab-${num}`).show();
+
+        for (var i = 1; i < 10; i++) {
+            if (i != num) {
+                $(`#tab-${i}`).hide();
+            }
+        }
+    });
 });
